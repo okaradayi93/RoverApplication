3e1548456a7081f3203859a8c89d46785839f808
Oğuzhan <oguzhankaradayi93@gmail.com>
2020-12-01 00:39:07 +0300

    Set position test

--- UnitTest/RotationTest.cs
+++ UnitTest/RotationTest.cs
@@ -10,9 +10,13 @@ namespace RoverApp
         {
             ParameterConfig = new ParameterConfig {
                 RoverDirectionMapDictionary = new System.Collections.Generic.Dictionary<char, RoverDirectionMap> {
-                { 'L', new RoverDirectionMap { DirectionValue = Enum.ControlAction.Left } }
+                    { 'L', new RoverDirectionMap { DirectionValue = Enum.ControlAction.Left }}
+                },
+                DirectionMapDictionary = new System.Collections.Generic.Dictionary<Enum.DirectionValue, Direction> {
+                    { Enum.DirectionValue.North, new Direction { HorizontalValue = Enum.MoveValue.Stay, VerticalValue = Enum.MoveValue.Next } } 
                 }
             };
+            ParameterConfig.RoverDirectionMapDictionary.Add('M', new RoverDirectionMap { PosionValue= Enum.PosionValue.Move } );
         }
 
         [Test]
@@ -29,6 +33,23 @@ namespace RoverApp
             
             rover.SetRoverDirection('L');
             Assert.AreEqual(rover.CurrentDirection, Enum.DirectionValue.West);
+        }
+
+        [Test]
+        public void PosionTest_Move_True()
+        {
+            Rover rover = new Rover
+            {
+                LimitX = 3,
+                LimitY = 3,
+                CurrentDirection = Enum.DirectionValue.North,
+                XCoordinate = 1,
+                YCoordinate = 2,
+                ParameterConfig = ParameterConfig
+            };
+
+            rover.SetRoverPosition('M');
+            Assert.AreEqual(rover.YCoordinate, 3);
         }
     }
 }
